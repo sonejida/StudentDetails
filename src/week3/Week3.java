495e693bab49b883fb3568a6847ec453ce96feb4
sonejida <sonejida@sheridancollege.ca>
2023-05-29 12:40:51 -0400

    version2

--- src/week3/Week3.java
+++ src/week3/Week3.java
@@ -16,6 +16,7 @@ public class Week3 {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Learning github operations");
+        System.out.println("push github operations");
     }
     
 }
